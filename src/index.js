87a8a56b0ae7c9662f74f04b5c95807c2319a4b4
iamidlek <iamidlek@naver.com>
2021-11-28 01:01:28 +0900

    기본 설정 및 동작 확인

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { Switch, Route } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import store, { history } from "./redux/store";
 
 ReactDOM.render(
   // <React.StrictMode>
-  <App />,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
   // </React.StrictMode>,
   document.getElementById("root")
 );
