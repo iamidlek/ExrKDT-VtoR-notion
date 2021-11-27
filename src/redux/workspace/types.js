18587795ee4f254af932d5e9abc5a556073062cc
iamidlek <iamidlek@naver.com>
2021-11-28 01:02:18 +0900

    기본 타입들 임시 작성

---
+++ src/redux/workspace/types.js
@@ -0,0 +1,5 @@
+export const CREATE_WS = "CREATE_WS";
+export const READ_WS_LIST = "READ_WS_LIST";
+export const READ_WS = "READ_WS";
+export const UPDATE_WS = "UPDATE_WS";
+export const DELETE_WS = "DELETE_WS";
