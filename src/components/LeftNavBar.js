fd5e9e6fe1d611d4a7ee3df24dac220402ed3f82
iamidlek <iamidlek@naver.com>
2021-11-29 01:03:47 +0900

    LNB 기본 골격

--- src/components/LeftNavBar.js
+++ src/components/LeftNavBar.js
@@ -1,30 +1,34 @@
+import styles from "./LeftNavBar.module.scss";
 import { useSelector } from "react-redux";
 import WorkspaceListItem from "./WorkspaceListItem";
+import { useState } from "react";
 
 export default function LeftNavBar() {
   const workspaces = useSelector((state) => state.workspaceTree.workspaceList);
+  const [navWidth, setNavWidth] = useState(240);
   return (
-    <div>
-      <nav>
-        <div className="header">
-          <div className="user-profile"></div>
-          Yhole's Notion
+    <nav style={{ width: `${navWidth}px` }}>
+      <div className={styles["header"]}>
+        <div className={styles["user-profile"]}></div>
+        Yhole's Notion
+      </div>
+      <ul>
+        {workspaces.map((item) => (
+          <WorkspaceListItem key={item.id} item={item} />
+        ))}
+      </ul>
+      <div className={styles["actions"]}>
+        <div className={styles["action"]}>
+          <span className={`material-icons ${styles["material-icons"]}`}>
+            add
+          </span>
+          새로운 페이지
         </div>
-        <ul>
-          {workspaces.map((item) => (
-            <WorkspaceListItem key={item.id} item={item} />
-          ))}
-        </ul>
-        <div className="actions">
-          <div className="action">
-            <span className="material-icons">add</span> 새로운 페이지
-          </div>
-        </div>
-        <div
-          className="resize-handle"
-          text="너비 자동 조절 라이브러리 사용할것"
-        ></div>
-      </nav>
-    </div>
+      </div>
+      <div
+        className={styles["resize-handle"]}
+        text="너비 자동 조절 라이브러리 사용할것"
+      ></div>
+    </nav>
   );
 }
