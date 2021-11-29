eff32ce5bb4bbaa7525e90ee1198baadcc5fe9c5
iamidlek <iamidlek@naver.com>
2021-11-30 00:25:40 +0900

    NotFound 스타일

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,9 @@
 export default function NotFound() {
-  return <div>Page Not Found</div>;
+  return (
+    <section style={{ textAlign: "center", padding: "100px 0 200px" }}>
+      <h1 style={{ fontSize: "60px", fontWeight: "700" }}>
+        404 Page Not Found!
+      </h1>
+    </section>
+  );
 }
