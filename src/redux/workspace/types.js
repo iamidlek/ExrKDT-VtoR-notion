566fb7b3aeff0b1af957370fd7cd3ba0caee791f
iamidlek <iamidlek@naver.com>
2021-11-30 00:24:26 +0900

    타입 정리

--- src/redux/workspace/types.js
+++ src/redux/workspace/types.js
@@ -1,11 +1,10 @@
 export const REQ_START = "REQ_START";
 export const REQ_START_WS = "REQ_START_WS";
-export const REQ_SUCCESS = "REQ_START";
 export const REQ_FAIL = "REQ_FAIL";
 export const REQ_FAIL_WS = "REQ_FAIL_WS";
 
-export const CREATE_WS = "CREATE_WS";
 export const READ_WS_LIST = "READ_WS_LIST";
 export const READ_WS = "READ_WS";
 export const UPDATE_WS = "UPDATE_WS";
-export const DELETE_WS = "DELETE_WS";
+
+export const MAKE_PATH = "MAKE_PATH";
